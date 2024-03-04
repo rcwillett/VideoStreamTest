3142acd19fa1a7d6e70080e6777cd0fa07bb704e
willitt <oblivion1214@hotmail.com>
2024-03-03 21:53:12 -0500

    moved media strem object to ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { useRef, useState } from 'react'
 import './App.css'
 
-let mediaStream: MediaStream;
-
 function App() {
   const videoElem = useRef<HTMLVideoElement>(null);
+  const mediaStream = useRef<MediaStream | null>(null);
 
   const [isStreaming, setStreaming] = useState(false);
 
   const startSteam = async () => {
     if (navigator.mediaDevices) {
-      mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+      mediaStream.current = await navigator.mediaDevices.getUserMedia({ video: true });
       
       if (videoElem.current) {
-        videoElem.current.srcObject = mediaStream;
+        videoElem.current.srcObject = mediaStream.current;
         videoElem.current.play();
       }
       setStreaming(true);
@@ -21,8 +20,8 @@ function App() {
   };
 
   const endStream = () => {
-    if (mediaStream) {
-      mediaStream.getTracks().forEach((track) => track.stop());
+    if (mediaStream.current) {
+      mediaStream.current.getTracks().forEach((track) => track.stop());
       setStreaming(false);
     }
     if (videoElem.current) {
